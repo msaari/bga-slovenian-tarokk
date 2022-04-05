394127cf17cd546b765f9f0970edada9c07bb799
Mikko Saari <mikko@mikkosaari.fi>
2022-04-05 06:51:18 +0300

    Actions: first steps

--- sloveniantarokk.action.php
+++ sloveniantarokk.action.php
@@ -2,12 +2,12 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * SlovenianTarokk implementation : © <Your name here> <Your email address here>
+ * SlovenianTarokk implementation : © Mikko Saari <mikko@mikkosaari.fi>
  *
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
  * -----
- * 
+ *
  * sloveniantarokk.action.php
  *
  * SlovenianTarokk main action entry point
@@ -15,51 +15,27 @@
  *
  * In this file, you are describing all the methods that can be called from your
  * user interface logic (javascript).
- *       
+ *
  * If you define a method "myAction" here, then you can call it from your javascript code with:
  * this.ajaxcall( "/sloveniantarokk/sloveniantarokk/myAction.html", ...)
- *
  */
 
-class action_sloveniantarokk extends APP_GameAction
-{ 
-    // Constructor: please do not modify
-   	public function __default()
-  	{
-  	    if( self::isArg( 'notifwindow') )
-  	    {
-            $this->view = "common_notifwindow";
-  	        $this->viewArgs['table'] = self::getArg( "table", AT_posint, true );
-  	    }
-  	    else
-  	    {
-            $this->view = "sloveniantarokk_sloveniantarokk";
-            self::trace( "Complete reinitialization of board game" );
-        }
-  	} 
-  	
-  	// TODO: defines your action entry points there
+class action_sloveniantarokk extends APP_GameAction {
+	// Constructor: please do not modify
+	public function __default() {
+		if( self::isArg( 'notifwindow' ) ) {
+			$this->view              = 'common_notifwindow';
+			$this->viewArgs['table'] = self::getArg( 'table', AT_posint, true );
+		} else {
+			$this->view = 'sloveniantarokk_sloveniantarokk';
+			self::trace( 'Complete reinitialization of board game' );
+		}
+	}
 
-
-    /*
-    
-    Example:
-  	
-    public function myAction()
-    {
-        self::setAjaxMode();     
-
-        // Retrieve arguments
-        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
-        $arg1 = self::getArg( "myArgument1", AT_posint, true );
-        $arg2 = self::getArg( "myArgument2", AT_posint, true );
-
-        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
-        $this->game->myAction( $arg1, $arg2 );
-
-        self::ajaxResponse( );
-    }
-    
-    */
-
+	public function playCard() {
+		self::setAjaxMode();
+		$card_id = self::getArg( 'id', AT_posint, true );
+		$this->game->playCard( $card_id );
+		self::ajaxResponse();
+	}
 }
