b64fd855b5a737c6e553121559117476b5d69b6c
Mikko Saari <mikko@mikkosaari.fi>
2022-04-05 14:56:04 +0300

    DB: Remove player_team parameter

--- dbmodel.sql
+++ dbmodel.sql
@@ -22,8 +22,6 @@ ALTER TABLE `player` ADD `player_score` INT(1) NOT NULL DEFAULT '0';
 
 ALTER TABLE `player` ADD `player_radl` INT(1) NOT NULL DEFAULT '0';
 
-ALTER TABLE `player` ADD `player_team` INT(1) NOT NULL DEFAULT '0';
-
 CREATE TABLE IF NOT EXISTS `card` (
   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `card_type` varchar(16) NOT NULL,
