21753b2e1c981848bdbf7ebd7a1ffb5732dc907a
Mikko Saari <mikko@mikkosaari.fi>
2022-04-05 06:07:57 +0300

    View basics

--- sloveniantarokk.view.php
+++ sloveniantarokk.view.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * SlovenianTarokk implementation : © <Your name here> <Your email address here>
+ * SlovenianTarokk implementation : © Mikko Saari <mikko@mikkosaari.fi>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
@@ -12,75 +12,55 @@
  *
  * This is your "view" file.
  *
- * The method "build_page" below is called each time the game interface is displayed to a player, ie:
+ * The method "build_page" below is called each time the game interface is
+ * displayed to a player, ie:
  * _ when the game starts
  * _ when a player refreshes the game page (F5)
  *
- * "build_page" method allows you to dynamically modify the HTML generated for the game interface. In
- * particular, you can set here the values of variables elements defined in sloveniantarokk_sloveniantarokk.tpl (elements
- * like {MY_VARIABLE_ELEMENT}), and insert HTML block elements (also defined in your HTML template file)
+ * "build_page" method allows you to dynamically modify the HTML generated for
+ * the game interface. In particular, you can set here the values of variables
+ * elements defined in sloveniantarokk_sloveniantarokk.tpl (elements like
+ * {MY_VARIABLE_ELEMENT}), and insert HTML block elements (also defined in your
+ * HTML template file)
  *
- * Note: if the HTML of your game interface is always the same, you don't have to place anything here.
- *
+ * Note: if the HTML of your game interface is always the same, you don't have
+ * to place anything here.
  */
 
 require_once( APP_BASE_PATH."view/common/game.view.php" );
 
-class view_sloveniantarokk_sloveniantarokk extends game_view
-{
-    function getGameName() {
-        return "sloveniantarokk";
-    }
-    
-  	function build_page( $viewArgs )
-  	{		
-  	    // Get players & players number
-        $players = $this->game->loadPlayersBasicInfos();
-        $players_nbr = count( $players );
+class view_sloveniantarokk_sloveniantarokk extends game_view {
+	function getGameName() {
+		return "sloveniantarokk";
+	}
 
-        /*********** Place your code below:  ************/
+	function build_page( $viewArgs ) {
+		// Get players & players number
+		$players     = $this->game->loadPlayersBasicInfos();
+		$players_nbr = count( $players );
 
+		$template = self::getGameName() . '_' . self::getGameName();
 
-        /*
-        
-        // Examples: set the value of some element defined in your tpl file like this: {MY_VARIABLE_ELEMENT}
+		$directions = array( 'S', 'W', 'N', 'E' );
 
-        // Display a specific number / string
-        $this->tpl['MY_VARIABLE_ELEMENT'] = $number_to_display;
+		$this->page->begin_block( $template, 'player' );
+		foreach ( $players as $player_id => $info ) {
+			$dir = array_shift( $directions );
 
-        // Display a string to be translated in all languages: 
-        $this->tpl['MY_VARIABLE_ELEMENT'] = self::_("A string to be translated");
+			$this->page->insert_block(
+				'player',
+				array (
+					'PLAYER_ID'    => $player_id,
+					'PLAYER_NAME'  => $players[ $player_id ]['player_name'],
+					'PLAYER_COLOR' => $players[ $player_id ]['player_color'],
+					'DIR'          => $dir
+				)
+			);
+		}
 
-        // Display some HTML content of your own:
-        $this->tpl['MY_VARIABLE_ELEMENT'] = self::raw( $some_html_code );
-        
-        */
-        
-        /*
-        
-        // Example: display a specific HTML block for each player in this game.
-        // (note: the block is defined in your .tpl file like this:
-        //      <!-- BEGIN myblock --> 
-        //          ... my HTML code ...
-        //      <!-- END myblock --> 
-        
+		$this->tpl['MY_HAND'] = self::_( 'My hand' );
 
-        $this->page->begin_block( "sloveniantarokk_sloveniantarokk", "myblock" );
-        foreach( $players as $player )
-        {
-            $this->page->insert_block( "myblock", array( 
-                                                    "PLAYER_NAME" => $player['player_name'],
-                                                    "SOME_VARIABLE" => $some_value
-                                                    ...
-                                                     ) );
-        }
-        
-        */
-
-
-
-        /*********** Do not change anything below this line  ************/
-  	}
+		/*
+		 ********** Do not change anything below this line  ************/
+	}
 }
-  
-
