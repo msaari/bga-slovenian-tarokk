615374ccdf62abebd82e8dbddb01cf2567358b85
Mikko Saari <mikko@mikkosaari.fi>
2022-04-11 14:27:21 +0300

    Material: bid point values

--- material.inc.php
+++ material.inc.php
@@ -140,3 +140,19 @@ $this->bid_names = array(
 	13 => clienttranslate( 'Valat' ),
 	'pass' => clienttranslate( 'Pass' ),
 );
+
+$this->bid_point_values = array(
+	1 => 70,
+	2 => 10,
+	3 => 20,
+	4 => 30,
+	5 => 40,
+	6 => 50,
+	7 => 60,
+	8 => 70,
+	9 => 80,
+	10 => 90,
+	11 => 125,
+	12 => 125,
+	13 => 500,
+);
