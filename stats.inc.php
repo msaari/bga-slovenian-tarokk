4c36bfb2dfd5fc6de458a6ddf0a0ea9ab63ebb9a
Mikko Saari <mikko@mikkosaari.fi>
2022-05-20 14:20:11 +0300

    Stats: Add stats

--- stats.inc.php
+++ stats.inc.php
@@ -3,7 +3,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * SlovenianTarokk implementation : © <Your name here> <Your email address here>
+ * SlovenianTarokk implementation : © Mikko Saari <mikko@mikkosaari.fi>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
@@ -12,84 +12,166 @@
  * stats.inc.php
  *
  * SlovenianTarokk game statistics description
- *
  */
 
-/*
-    In this file, you are describing game statistics, that will be displayed at the end of the
-    game.
-    
-    !! After modifying this file, you must use "Reload  statistics configuration" in BGA Studio backoffice
-    ("Control Panel" / "Manage Game" / "Your Game")
-    
-    There are 2 types of statistics:
-    _ table statistics, that are not associated to a specific player (ie: 1 value for each game).
-    _ player statistics, that are associated to each players (ie: 1 value for each player in the game).
-
-    Statistics types can be "int" for integer, "float" for floating point values, and "bool" for boolean
-    
-    Once you defined your statistics there, you can start using "initStat", "setStat" and "incStat" method
-    in your game logic, using statistics names defined below.
-    
-    !! It is not a good idea to modify this file when a game is running !!
-
-    If your game is already public on BGA, please read the following before any change:
-    http://en.doc.boardgamearena.com/Post-release_phase#Changes_that_breaks_the_games_in_progress
-    
-    Notes:
-    * Statistic index is the reference used in setStat/incStat/initStat PHP method
-    * Statistic index must contains alphanumerical characters and no space. Example: 'turn_played'
-    * Statistics IDs must be >=10
-    * Two table statistics can't share the same ID, two player statistics can't share the same ID
-    * A table statistic can have the same ID than a player statistics
-    * Statistics ID is the reference used by BGA website. If you change the ID, you lost all historical statistic data. Do NOT re-use an ID of a deleted statistic
-    * Statistic name is the English description of the statistic as shown to players
-    
-*/
-
 $stats_type = array(
-
-    // Statistics global to table
-    "table" => array(
-
-        "turns_number" => array("id"=> 10,
-                    "name" => totranslate("Number of turns"),
-                    "type" => "int" ),
-
-/*
-        Examples:
-
-
-        "table_teststat1" => array(   "id"=> 10,
-                                "name" => totranslate("table test stat 1"), 
-                                "type" => "int" ),
-                                
-        "table_teststat2" => array(   "id"=> 11,
-                                "name" => totranslate("table test stat 2"), 
-                                "type" => "float" )
-*/  
-    ),
-    
-    // Statistics existing for each player
-    "player" => array(
-
-        "turns_number" => array("id"=> 10,
-                    "name" => totranslate("Number of turns"),
-                    "type" => "int" ),
-    
-/*
-        Examples:    
-        
-        
-        "player_teststat1" => array(   "id"=> 10,
-                                "name" => totranslate("player test stat 1"), 
-                                "type" => "int" ),
-                                
-        "player_teststat2" => array(   "id"=> 11,
-                                "name" => totranslate("player test stat 2"), 
-                                "type" => "float" )
-
-*/    
-    )
-
+	'table'  => array(
+		'bid_klop' => array(
+			'id'   => 10,
+			'name' => totranslate( 'Klop' ),
+			'type' => 'int',
+		),
+		'bid_three' => array(
+			'id'   => 11,
+			'name' => totranslate( 'Three' ),
+			'type' => 'int',
+		),
+		'bid_two' => array(
+			'id'   => 12,
+			'name' => totranslate( 'Two' ),
+			'type' => 'int',
+		),
+		'bid_one' => array(
+			'id'   => 13,
+			'name' => totranslate( 'One' ),
+			'type' => 'int',
+		),
+		'bid_solo_three' => array(
+			'id'   => 14,
+			'name' => totranslate( 'Solo 3' ),
+			'type' => 'int',
+		),
+		'bid_solo_two' => array(
+			'id'   => 15,
+			'name' => totranslate( 'Solo 2' ),
+			'type' => 'int',
+		),
+		'bid_solo_one' => array(
+			'id'   => 16,
+			'name' => totranslate( 'Solo 1' ),
+			'type' => 'int',
+		),
+		'bid_beggar' => array(
+			'id'   => 17,
+			'name' => totranslate( 'Beggar' ),
+			'type' => 'int',
+		),
+		'bid_solo_without' => array(
+			'id'   => 18,
+			'name' => totranslate( 'Solo withouy' ),
+			'type' => 'int',
+		),
+		'bid_open_beggar' => array(
+			'id'   => 19,
+			'name' => totranslate( 'Open beggar' ),
+			'type' => 'int',
+		),
+		'bid_colour_valat_without' => array(
+			'id'   => 20,
+			'name' => totranslate( 'Colour valat without' ),
+			'type' => 'int',
+		),
+		'bid_colour_valat' => array(
+			'id'   => 21,
+			'name' => totranslate( 'Colour valat' ),
+			'type' => 'int',
+		),
+		'bid_valat' => array(
+			'id'   => 22,
+			'name' => totranslate( 'Valat' ),
+			'type' => 'int',
+		),
+	),
+	'player' => array(
+		'games_declared' => array(
+			'id'   => 10,
+			'name' => totranslate( 'Games declared' ),
+			'type' => 'int',
+		),
+		'games_as_partner' => array(
+			'id'   => 11,
+			'name' => totranslate( 'Games as a partner' ),
+			'type' => 'int',
+		),
+		'hands_won' => array(
+			'id'   => 12,
+			'name' => totranslate( 'Hands won as declarer' ),
+			'type' => 'int',
+		),
+		'hands_lost' => array(
+			'id'   => 13,
+			'name' => totranslate( 'Hands lost as declarer' ),
+			'type' => 'int',
+		),
+		'monds_captured' => array(
+			'id'   => 14,
+			'name' => totranslate( 'Monds captured' ),
+			'type' => 'int',
+		),
+		'monds_lost' => array(
+			'id'   => 15,
+			'name' => totranslate( 'Monds lost' ),
+			'type' => 'int',
+		),
+		'emperor_tricks_taken' => array(
+			'id'   => 16,
+			'name' => totranslate( "Emperor's tricks taken" ),
+			'type' => 'int',
+		),
+		'tricks_taken' => array(
+			'id'   => 17,
+			'name' => totranslate( 'Tricks taken' ),
+			'type' => 'int',
+		),
+		'radli_cleared' => array(
+			'id'   => 18,
+			'name' => totranslate( 'Radli cleared' ),
+			'type' => 'int',
+		),
+		'kings_in_talon' => array(
+			'id'   => 19,
+			'name' => totranslate( 'Called king in talon' ),
+			'type' => 'int',
+		),
+		'beggars_played' => array(
+			'id'   => 20,
+			'name' => totranslate( 'Beggar games played' ),
+			'type' => 'int',
+		),
+		'beggars_won' => array(
+			'id'   => 21,
+			'name' => totranslate( 'Beggar games won' ),
+			'type' => 'int',
+		),
+		'solos_played' => array(
+			'id'   => 22,
+			'name' => totranslate( 'Solo games played' ),
+			'type' => 'int',
+		),
+		'solos_won' => array(
+			'id'   => 23,
+			'name' => totranslate( 'Solo games won' ),
+			'type' => 'int',
+		),
+		'valats_played' => array(
+			'id'   => 24,
+			'name' => totranslate( 'Valat games played' ),
+			'type' => 'int',
+		),
+		'valats_won' => array(
+			'id'   => 25,
+			'name' => totranslate( 'Valat games won' ),
+			'type' => 'int',
+		),
+		'announcements_made' => array(
+			'id'   => 26,
+			'name' => totranslate( 'Announcements made' ),
+			'type' => 'int',
+		),
+		'announcements_kontrad' => array(
+			'id'   => 27,
+			'name' => totranslate( 'Announcements kontrad' ),
+			'type' => 'int',
+		),
+	),
 );
